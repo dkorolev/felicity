34c7e5483e08c1f18f014d1d355ebd32c768d04b
Dima <dmitry.korolev@gmail.com>
2014-01-22 21:51:11 -0800

    git add date_now.h

---
+++ date_now.h
@@ -0,0 +1,19 @@
+// date_now() returns the number of milliseconds elapsed since 1 January 1970 00:00:00 UTC.
+// Mimics Date.now() from JavaScript/node.js.
+//
+// https://twitter.com/UniqueDima/status/420274974373343232
+
+#ifndef FELICITY_DATE_NOW
+#define FELICITY_DATE_NOW
+
+#include <chrono>
+
+namespace felicity {
+
+int64_t date_now() {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+}  // namespace felicity
+
+#endif
